1adfb9b9bab60e9bb2f81583de6df3373686dc03
Matheus Rosa <matheusrosataxa@gmail.com>
2024-08-10 20:14:11 -0300

    feat: search_by_date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 # Requisito 7
+from datetime import datetime
 from tech_news.database import search_news
 
 
@@ -13,8 +14,15 @@ def search_by_title(title):
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    try:
+        iso_date = datetime.strptime(date, "%Y-%m-%d")
+        formatted_date = iso_date.strftime("%d/%m/%Y")
+    except ValueError:
+        raise ValueError("Data inválida")
+    return [
+        (notice["title"], notice["url"])
+        for notice in search_news({"timestamp": formatted_date})
+    ]
 
 
 # Requisito 9
