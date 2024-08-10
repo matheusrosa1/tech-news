9afd9e0c2d921e7505578eb0a1fd99a98361e177
Matheus Rosa <matheusrosataxa@gmail.com>
2024-08-10 20:56:45 -0300

    feat: top_5_categories

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,18 @@
-# Requisito 10
+from collections import Counter
+from tech_news.database import search_news
+
+
 def top_5_categories():
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    all_news = search_news({})
+
+    categories = [news["category"] for news in all_news if "category" in news]
+
+    category_counts = Counter(categories)
+
+    sorted_categories = sorted(
+        category_counts.items(), key=lambda item: (-item[1], item[0])
+    )
+
+    top_categories = [category for category, count in sorted_categories]
+
+    return top_categories[:5]
